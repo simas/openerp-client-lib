ee301baf848d0d26cb0360fb623ada2a03872ed7
Simas Skrebiškis <simas.skrebiskis@gmail.com>
2015-09-16 17:43:38 +0300

    xmlrpclib is included in py3 as xmlrpc.client

--- openerplib/main.py
+++ openerplib/main.py
@@ -35,7 +35,7 @@ Home page: http://pypi.python.org/pypi/openerp-client-lib
 Code repository: https://code.launchpad.net/~niv-openerp/openerp-client-lib/trunk
 """
 
-import xmlrpclib
+from xmlrpc import client as xmlrpclib
 import logging
 import json
 import urllib
